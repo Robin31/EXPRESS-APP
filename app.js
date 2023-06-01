9588f02b7f6a1c6de09ffb9b31641f68905004d5
Robin31 <robindenis@hotmail.fr>
2023-06-01 16:36:35 +0200

    Quete express 4 bis

--- app.js
+++ app.js
@@ -14,14 +14,20 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
+const { validateUser } = require("./validateUser.js");
+const { validateMovie } = require("./validateMovie.js");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateUsers);
 
 const usersHandlers = require("./usersHandlers");
 
 app.get("/api/users", usersHandlers.getUsers);
 app.get("/api/users/:id", usersHandlers.getUsersById);
+app.post("/api/users", validateUser, movieHandlers.postUsers);
+app.put("/api/users/:id", validateUser, movieHandlers.updateUsers);
 
 app.listen(port, (err) => {
   if (err) {
